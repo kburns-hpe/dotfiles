7ab944cfff1c27746fcfc2ee983d8692bb0369c6
Soleblaze <soleblaze@exploits.sexy>
2014-11-28 14:50:17 -0600

    Initial Commit

---
+++ setup_awesome.zsh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+host="$1"
+confdir="$PWD/awesome"
+
+mkdir -p ~/.config/awesome/images 2>/dev/null
+mkdir -p ~/.config/awesome/themes 2>/dev/null
+mkdir -p ~/.fonts 2>/dev/null
+
+# Link fonts
+ln -s "$confdir/fonts/BITSUMIS.TTF" ~/.fonts/BITSUMIS.TTF
+ln -s "$confdir/fonts/CGF Locust Resistance.ttf" ~/.fonts/"CGF Locust Resistance.ttf"
+fc-cache
+
+# Link main configurations
+pushd "$confdir"
+for file in bindings eminent menu notification rules signals tags tasklist widgets; do
+    ln -s "$confdir/$file.lua" "~/.config/awesome/$file.lua"
+done
+
+# Copy config file
+cp "$confdir/config.lua" ~/.config/awesome/config.lua
+
+# Link themes
+pushd "$confdir/themes"
+for file in *.lua; do
+    ln -s "$confdir/themes/$file" ~/.config/awesome/themes/"$file"
+done
+popd
+
+# Link scripts
+ln -s "$confdir/df.sh" ~/.config/awesome/df.sh
+
+# Link images
+for image in $(cd "$confdir/images"; ls); do
+    ln -s "$confdir/images/$image" ~/.config/awesome/images/$image
+done
