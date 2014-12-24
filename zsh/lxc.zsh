e0b5dd92636996e2e9285a5a28853dccc549184c
Soleblaze <soleblaze@exploits.sexy>
2014-12-23 18:41:34 -0600

    Initial commit for lxc aliases

---
+++ zsh/lxc.zsh
@@ -0,0 +1,41 @@
+#!/usr/bin/zsh
+
+# Fix keyboard after lxc starts
+alias fixkb="setxkbmap -option terminate:ctrl_alt_bksp; xmodmap ~/.Xmodmap; xcape -e 'Control_R=Escape'"
+
+funciton lxcs () {
+    sudo lxc-start -n $1 -d
+    echo "Fixing keyboard..."
+    sleep 5 && setxkbmap -option terminate:ctrl_alt_bksp; xmodmap ~/.Xmodmap; xcape -e 'Control_R=Escape'
+}
+
+alias lxcstop="sudo lxc-stop -n"
+alias lxcls="sudo lxc-ls -f"
+
+function lxcc ()
+{
+    machine=$1
+    clone=$2
+    
+    if [ -d "/var/lib/lxc/$machine" ]; then
+        if [ "$clone" ]; then
+            sudo lxc-clone -s -o "$machine" -n "$clone"
+            sudo sed -i -e 's|\(^lxc.mount.entry.*/var/lib/lxc/\)'$machine'|\1'$clone\| "/var/lib/lxc/$clone/config"
+        else
+            echo "Error: Clone machine name not specified"
+        fi
+    else
+        echo "$machine is not a valid lxc container"
+    fi
+}
+
+function lxcd ()
+{
+    machine=$1
+    if [ "$machine" != "tools" -a "$machine" != "target-arch" -a "$machine" \
+        != "target-ubuntu" ]; then
+        sudo lxc-destroy -n $machine
+    else
+        echo "Error: $machine is a template and should not be destroyed."
+    fi
+}
