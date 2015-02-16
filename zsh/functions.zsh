804d040b01422e965590b31c635b838a6b6eedca
Soleblaze <soleblaze@skyshadows.net>
2015-02-15 21:23:45 -0600

    added ssh functions to change tmux window names

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -15,3 +15,62 @@ function convertcolor() {
 function catnc() {
     egrep -v '(^#|^/)' "$1" | sed '/^$/d'
 }
+
+# Renames tmux window to server ssh is being used to connect to
+function ssh() {
+  if [ -n "$TMUX" ] # set only if within running tmux
+  then
+    window_index=$(tmux display-message -p '#I')
+
+    # arbitrary environment variable name to remember ssh args like server
+    # hostname so we can connect again.
+    session_variable_name="window_${window_index}_ssh_args"
+
+    # save in tmux session variable
+    tmux setenv $session_variable_name "$*"
+
+    # set window title
+    tmux rename-window $*
+
+    # run ssh
+    /usr/bin/ssh $*
+
+    # unset variable so new panes don't continue ssh-ing to this server
+    tmux setenv -u $session_variable_name
+  else
+    /usr/bin/ssh $*
+  fi
+
+  # rename title back
+  tmux rename-window zsh
+}
+
+# Renames tmux window to server ssh is being used to connect to
+# Doesn't save key in known_hosts
+function sshnh() {
+  if [ -n "$TMUX" ] # set only if within running tmux
+  then
+    window_index=$(tmux display-message -p '#I')
+
+    # arbitrary environment variable name to remember ssh args like server
+    # hostname so we can connect again.
+    session_variable_name="window_${window_index}_ssh_args"
+
+    # save in tmux session variable
+    tmux setenv $session_variable_name "$*"
+
+    # set window title
+    tmux rename-window $*
+
+    # run ssh
+    /usr/bin/ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" $*
+
+    # unset variable so new panes don't continue ssh-ing to this server
+    tmux setenv -u $session_variable_name
+  else
+    /usr/bin/ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" $*
+  fi
+
+  # rename title back
+  tmux rename-window zsh
+}
