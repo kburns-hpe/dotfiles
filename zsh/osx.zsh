c189b3781acff9828cf3f8c3d505b289da7b8165
Kevin Burns <soleblaze@Fairlight-Excalibur.local>
2015-07-05 23:28:17 -0500

    using java_home causes an error when java isn't installed

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -9,4 +9,4 @@ export PATH="$HOME/bin::/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
 
 # Over-ride ls alias
 alias ls="ls -CGFh"
-export JAVA_HOME=$(/usr/libexec/java_home)
+#export JAVA_HOME=$(/usr/libexec/java_home)
