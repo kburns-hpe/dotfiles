4162b6173548f83c6c2f45d7781ef919f273ba47
soleblaze <soleblaze@exploits.sexy>
2015-07-13 22:12:19 -0500

    added chefkdk and brew zsh-completions paths

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -6,6 +6,10 @@ export EDITOR=vim
 
 # Set Path
 export PATH="$HOME/bin::/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
+export PATH="$PATH:~/.chefdk/gem/ruby/2.1.0/bin"
+
+# Set fpath for zsh-completion (homebrew package)
+fpath=(/usr/local/share/zsh-completions $fpath)
 
 # Over-ride ls alias
 alias ls="ls -CGFh"
