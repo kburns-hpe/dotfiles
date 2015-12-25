0f47c37e3cbb1204a101ed982437a32bf96eebdb
soleblaze <soleblaze@exploits.sexy>
2015-12-24 18:54:58 -0600

    Added functions to select recoll items

--- zsh/recoll.zsh
+++ zsh/recoll.zsh
@@ -5,3 +5,24 @@ alias rdt="recoll -d -t"
 alias rpdf="recoll -t ext:pdf"
 alias rtxt="recoll -t ext:txt"
 alias rmd="recoll -t ext:md"
+
+function ort {
+    xdg-open "$(recoll -t "$*" | grep file | awk -F'\t' '{print $2}' |\
+    tr -d '[]' | sed 's|file://||' | peco)"
+}
+function ordt {
+    xdg-open "$(recoll -d -t "$*" | grep file | awk -F'\t' '{print $2}' |\
+    tr -d '[]' | sed 's|file://||' | peco)"
+}
+function orpdf {
+    xdg-open "$(recoll -t ext:pdf "$*" | grep file | awk -F'\t' '{print $2}' |\
+    tr -d '[]' | sed 's|file://||' | peco)"
+}
+function ortxt {
+    xdg-open "$(recoll -t ext:txt "$*" | grep file | awk -F'\t' '{print $2}' |\
+    tr -d '[]' | sed 's|file://||' | peco)"
+}
+function ormd {
+    xdg-open "$(recoll -t ext:md "$*" | grep file | awk -F'\t' '{print $2}' |\
+    tr -d '[]' | sed 's|file://||' | peco)"
+}
