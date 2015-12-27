07fc9f14152ee3d3c2135809f8e2382346d564aa
soleblaze <soleblaze@exploits.sexy>
2015-12-26 23:04:54 -0600

    updated to work with different sinks
    Took this from the i3blocks volume widget and modified it to be used via
    the command line.

--- i3/pacvol.sh
+++ i3/pacvol.sh
@@ -1,122 +1,58 @@
-#!/bin/sh
-# PulseAudio Volume Control Script
-#   2010-05-20 - Gary Hetzel <garyhetzel@gmail.com>
-#
-#   BUG:    Currently doesn't get information for the specified sink,
-#           but rather just uses the first sink it finds in list-sinks
-#           Need to fix this for systems with multiple sinks
-#
+#!/bin/bash
+# Copyright (C) 2014 Julien Bonjean <julien@bonjean.info>
+# Copyright (C) 2014 Alexander Keller <github@nycroth.com>
 
-SINK=0
-STEP=1
-MAXVOL=65537 # let's just assume this is the same all over
-MUTED=0
-#MAXVOL=`pacmd list-sinks | grep "volume steps" | cut -d: -f2 | tr -d "[:space:]"`
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-MUTED=`pacmd list-sinks 0 | grep muted | cut -d ' ' -f 2`
-#VOLPERC=`pactl list sinks | awk '/Volume: 0:/ {print substr($3, 1, index($3, "%") - 1)}' | head -n1`
-VOLPERC=`pactl list sinks | awk '/Volume: front-left:/ {print substr($5, 1, index($5, "%") - 1)}'`
-SKIPOVERCHECK=1
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-display(){
-  if [ "$MUTED" = "yes" ]; then
-    echo "🔇  muted"
-  elif [ "$VOLPERC" -lt 33 ]; then
-    echo "🔈  ${VOLPERC}%"
-  elif [ "$VOLPERC" -lt 66 ]; then
-    echo "🔉  ${VOLPERC}%"
-  else
-    echo "🔊  ${VOLPERC}%"
-  fi
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#------------------------------------------------------------------------
+
+# The second parameter overrides the mixer selection
+# For PulseAudio users, use "pulse"
+# For Jack/Jack2 users, use "jackplug"
+# For ALSA users, you may use "default" for your primary card
+# or you may use hw:# where # is the number of the card desired
+MIXER="pulse"
+
+# The instance option sets the control to report and configure
+# This defaults to the first control of your selected mixer
+# For a list of the available, use `amixer -D $Your_Mixer scontrols`
+SCONTROL="${BLOCK_INSTANCE:-$(amixer -D $MIXER scontrols |
+                  sed -n "s/Simple mixer control '\([A-Za-z ]*\)',0/\1/p" |
+                  head -n1
+                )}"
+
+# The first parameter sets the step to change the volume by (and units to display)
+# This may be in in % or dB (eg. 5% or 3dB)
+STEP="5%"
+
+#------------------------------------------------------------------------
+
+capability() { # Return "Capture" if the device is a capture device
+  amixer -D "$MIXER" get "$SCONTROL" |
+    sed -n "s/  Capabilities:.*cvolume.*/Capture/p"
 }
 
-up(){
-	VOLSTEP="$(( $VOLPERC+$STEP ))";
+volume() {
+  amixer -D "$MIXER" get "$SCONTROL" "$(capability)"
 }
 
-down(){
-	VOLSTEP="$(( $VOLPERC-$STEP ))";
-	SKIPOVERCHECK=1
-}
+#------------------------------------------------------------------------
 
-max(){
-	pacmd set-sink-volume $SINK $MAXVOL > /dev/null
-}
-
-min(){
-	pacmd set-sink-volume $SINK 0 > /dev/null
-}
-
-overmax(){
-	SKIPOVERCHECK=1
-	if [ $VOLPERC -lt 100 ]; then
-		max;
-		exit 0;
-	fi
-	up
-}
-
-mute(){
-	pacmd set-sink-mute $SINK 1 > /dev/null
-}
-
-unmute(){
-	pacmd set-sink-mute $SINK 0 > /dev/null
-}
-
-toggle(){
-	M=`pacmd list-sinks | grep "muted" | cut -d: -f2 | tr -d "[:space:]"`
-	if [ $M == "no" ]; then
-		mute;
-	else
-		unmute;
-	fi
-}
-
-case $1 in
-up)
-	up;;
-down)
-	down;;
-max)
-	max
-	exit 0;;
-min)
-	min
-	exit 0;;
-overmax)
-	overmax;;
-toggle)
-	toggle
-	exit 0;;
-mute)
-	mute;
-	exit 0;;
-unmute)
-	unmute;
-	exit 0;;
-display)
-	display;
-	exit 0;;
-*)
-	echo "Usage: `basename $0` [up|down|min|max|overmax|toggle|mute|unmute|display]"
-	exit 1;;
+case "$1" in
+  toggle) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" toggle ;;  # right click, mute/unmute
+  up) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" "${STEP}+" unmute ;; # scroll up, increase
+  down) amixer -q -D "$MIXER" sset "$SCONTROL" "$(capability)" "${STEP}-" unmute ;; # scroll down, decrease
+  *) echo "Usage: $(basename "$0") [up|down|toggle]"
+    exit 1;;
 esac
-
-VOLUME="$(( ($MAXVOL/100) * $VOLSTEP ))"
-
-echo "$VOLUME : $OVERMAX"
-
- if [ -z $SKIPOVERCHECK ]; then
- 	if [ $VOLUME -gt $MAXVOL ]; then
- 		VOLUME=$MAXVOL
- 	elif [ $VOLUME -lt 0 ]; then
- 		VOLUME=0
- 	fi
- fi
-
-#echo "$VOLUME: $MAXVOL/100 * $VOLPERC+$VOLSTEP"
-pacmd set-sink-volume $SINK $VOLUME > /dev/null
-# VOLPERC=`pacmd list-sinks | grep "volume" | head -n1 | cut -d: -f3 | cut -d% -f1 | tr -d "[:space:]"`
-
-#osd_cat -b percentage -P $VOLPERC --delay=1 --align=center --pos bottom --offset 50 --color=green&
