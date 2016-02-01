1ea17de6309b457b75af538770dd3d5ba37c3d02
soleblaze <soleblaze@exploits.sexy>
2016-02-01 15:27:59 -0600

    fixed i3 link creation

--- setup_i3.zsh
+++ setup_i3.zsh
@@ -5,6 +5,7 @@ confdir="$PWD/i3"
 mkdir -p ~/.config/i3
 
 # Link files
-ln -s "${confdir}/config ~/.config/i3/config"
-ln -s "${confdir}/pacvol.sh ~/.config/i3/pacvol.sh"
-ln -s "${confdir}/i3blocks ~/.config/blocks"
+ln -s "${confdir}/config" ~/.config/i3/config
+ln -s "${confdir}/pacvol.sh" ~/.config/i3/pacvol.sh
+ln -s "${confdir}/i3blocks/scripts" ~/.config/blocks
+cp "${confdir}/i3blocks/i3blocks.conf ~/.i3blocks.conf
