2e1545bbd6e74d36f89ef686adc137a7ff1799df
soleblaze <soleblaze@exploits.sexy>
2017-01-12 22:24:23 -0600

    update paths

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -6,7 +6,7 @@ export EDITOR="NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
 
 # Set Path
 export PATH="$HOME/bin::/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
-export PATH="$PATH:$HOME/.chefdk/gem/ruby/2.1.0/bin:$HOME/.gem/ruby/2.0.0/bin:$HOME/node_modules/.bin"
+export PATH="$PATH:$HOME/Library/Python/2.7/bin:$HOME/node_modules/.bin"
 
 # Set fpath for zsh-completion (homebrew package)
 fpath=(/usr/local/share/zsh-completions $fpath)
