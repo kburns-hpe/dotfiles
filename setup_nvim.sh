5c90d82960de4c3f8199e589e9463c5314b79d49
soleblaze <soleblaze@exploits.sexy>
2017-02-03 15:14:10 -0600

    update nvim setup script

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-host="$1"
 confdir="$PWD/nvim"
 
 
 # Setup nvim directory
 mkdir -p ~/.config/nvim/{bundle,tmp,backup,colors,ftplugin,UltiSnips} 2>/dev/null
-ln -s $confdir/colors/cobalt.vim ~/.config/nvim/colors/cobalt.vim
+ln -s "$confdir"/colors/cobalt.vim ~/.config/nvim/colors/cobalt.vim
 
 # Setup init.vim
 ln -s "$confdir/init.vim" ~/.config/nvim/init.vim
@@ -21,6 +20,9 @@ for file in *; do
         ln -s "$confdir"/ftplugin/"$file" ~/.config/nvim/ftplugin/"$file"
 done
 popd
+
+# Setup UltiSnips directory
+ln -s "$confdir"/UltiSnips ~/.config/nvim/UltiSnips
 
 # Install plugins
 nvim -c "PlugInstall" -c ":qa"
