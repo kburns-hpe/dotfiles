a6b4f1014cf6d92e4387f7285b94fb5155be7d36
Soleblaze <soleblaze@exploits.sexy>
2017-03-10 14:06:39 +0000

    Fix setup i3 script

--- setup_i3.zsh
+++ setup_i3.zsh
@@ -7,5 +7,5 @@ mkdir -p ~/.config/i3
 # Link files
 ln -s "${confdir}/config" ~/.config/i3/config
 ln -s "${confdir}/pacvol.sh" ~/.config/i3/pacvol.sh
-ln -s "${confdir}/i3blocks/scripts" ~/.config/blocks
-cp "${confdir}/i3blocks/i3blocks.conf ~/.i3blocks.conf
+ln -s "${confdir}/i3blocks/scripts" ~/.config/i3/blocks
+cp "${confdir}/i3blocks/i3blocks.conf" ~/.i3blocks.conf
