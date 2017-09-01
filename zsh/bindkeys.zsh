40c156eb3b9e5bedf022fad3ad988857e1927eed
Kevin Burns <kevin.burns@netspi.com>
2017-09-01 17:32:01 -0500

    Add another set of binds for history-substring-search

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -4,6 +4,8 @@ bindkey -v
 zmodload zsh/terminfo
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
 
 #bind keys
 bindkey "\e[1~" beginning-of-line # Home
