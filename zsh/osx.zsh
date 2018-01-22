47bfe54bf4ed7200b1f41448756c9192cbffa5be
soleblaze <soleblaze@exploits.sexy>
2018-01-22 12:39:59 -0600

    Add Python 3.6 binaries to path

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -7,6 +7,7 @@ export EDITOR="nvim"
 # Set Path
 export PATH="$HOME/bin::/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
 export PATH="$PATH:$HOME/Library/Python/2.7/bin:$HOME/node_modules/.bin"
+export PATH="$PATH:$HOME/Library/Python/3.6/bin/"
 export PATH="$PATH:/Users/soleblaze/.gem/ruby/2.3.0/bin"
 
 # Set fpath for zsh-completion (homebrew package)
