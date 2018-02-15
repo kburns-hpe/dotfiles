b4ad38dd4499a7e18efe55e9f17550f07894913b
soleblaze <soleblaze@exploits.sexy>
2018-02-14 23:22:47 -0600

    Add termux-specific zsh config

---
+++ zsh/termux.zsh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+# Set Variables
+export BROWSER=termux-open-url
+export EDITOR=nvim
+
+# Set Path
+export PATH="$HOME/.local/bin:$HOME/.gem/ruby/2.3.0/bin:$HOME/bin:$PATH:$HOME/go/bin"
+
+# Linux LS aliases
+alias ls="ls --color=auto -CFh"
+alias sl="ls"
+alias ll="ls -l"
+alias lx="ls -X"
+alias lv="ls -v"
+
+# Highlight a specific search and pipe it through less
+alias hla='ack-grep -i --passthru --color --pager="less -R"'
+
+# Only show the matching lines, highlighted with a color
+alias hl='ack-grep -i --color --pager="less -R"'
+
+# Find a specific keyword in all files in current directory and highlight it
+
+function shl() {
+    searchfor="$1"
+    if [ "$2" ]; then
+        highlight="$2"
+    else
+        highlight="$1"
+    fi
+
+    find . -type f -exec egrep -i "$searchfor" {} /dev/null \; | ack-grep -i --passthru --color --pager="less -R" "$highlight"
+}
+
+# Other aliases
+alias open="xdg-open"
+alias xqf="xdg-mime query filetype"
+alias xqd="xdg-mime query default"
+alias xd="xdg-mime default"
+alias scpnh='scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
+
