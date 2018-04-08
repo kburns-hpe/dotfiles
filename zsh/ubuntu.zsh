18663cecf2ae76b246b2143f6b5f755d49a6797d
soleblaze <soleblaze@exploits.sexy>
2018-04-07 23:12:10 -0500

    Add GOPATH

--- zsh/ubuntu.zsh
+++ zsh/ubuntu.zsh
@@ -6,6 +6,7 @@ export EDITOR=nvim
 
 # Set Path
 export PATH="$HOME/.local/bin:$HOME/.gem/ruby/2.3.0/bin:$HOME/bin:$PATH:$HOME/go/bin"
+export GOPATH="$HOME/go"
 
 # Linux LS aliases
 alias ls="ls --color=auto -CFh"
