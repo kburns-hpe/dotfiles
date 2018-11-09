8892b76d179b8792e67ba5f7ea2a0be8d62ba04b
soleblaze <soleblaze@exploits.sexy>
2018-11-09 16:21:39 -0600

    Change colors to be darker

--- misc/atomonelight.js
+++ misc/atomonelight.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#383a42","#e45649","#50a14f","#c18401","#4078f2","#a626a4","#0184bc","#a0a1a7","#696c77","#e45649","#50a14f","#c18401","#4078f2","#a626a4","#0184bc","#fafafa"]);
-t.prefs_.set('foreground-color', "#383a42");
-t.prefs_.set('background-color', "#fafafa");
-t.prefs_.set('cursor-color', "#526eff");
+t.prefs_.set('color-palette-overrides',["#000000","#de3e35","#3f953a","#d2b67c","#2f5af3","#950095","#3f953a","#bbbbbb","#696c77","#de3e35","#3f953a","#d2b67c","#2f5af3","#a00095","#3f953a","#ffffff"]);
+t.prefs_.set('foreground-color', "#2a2c33");
+t.prefs_.set('background-color', "#f9f9f9");
+t.prefs_.set('cursor-color', "#bbbbbb");
