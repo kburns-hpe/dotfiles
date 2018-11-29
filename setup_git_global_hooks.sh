340af38a3e79b99ad68e573e9777c8ede061106c
soleblaze <soleblaze@exploits.sexy>
2018-11-28 21:46:22 -0600

    Update script to link files instead of dir

--- setup_git_global_hooks.sh
+++ setup_git_global_hooks.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 hook_dir="$PWD/git-hooks"
-mkdir ~/.git_template
-ln -s "$hook_dir" ~/.git_global_hooks
+mkdir ~/.git_global_hooks
+
+pushd $hook_dir
+for hook in *; do
+  ln -s "$hook_dir/$hook" ~/.git_global_hooks/"$hook"
+done
+popd
 
 git config --global core.hooksPath '~/.git_global_hooks'
 git config --global alias.ctags '!.git/hooks/ctags'
