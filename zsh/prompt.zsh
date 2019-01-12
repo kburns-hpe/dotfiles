70c82bd7d08ed3ff1b2e225eb2176275e81c42dd
soleblaze <soleblaze@exploits.sexy>
2019-01-11 23:25:10 -0600

    Switch prompt from pure to spaceship async

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,51 @@
-fpath=("$HOME/node_modules/pure-prompt/functions" $fpath)
+fpath=("$HOME/.zsh/prompt" $fpath)
+autoload -Uz async
 autoload -U promptinit
+async
 promptinit
-prompt pure
+
+
+SPACESHIP_PROMPT_ORDER=(
+  dir
+  git_branch
+  git_status
+#  kubecontext
+#  azure
+  exec_time
+  line_sep
+  jobs
+  char
+)
+
+SPACESHIP_RPROMPT_ORDER=(
+  time
+)
+
+# time
+SPACESHIP_TIME_FORMAT="%D{%L:%M}"
+SPACESHIP_TIME_SHOW=true
+
+# dir
+SPACESHIP_DIR_PREFIX=''
+SPACESHIP_DIR_TRUNC='5'
+SPACESHIP_DIR_TRUNC_PREFIX=".../"
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# git
+SPACESHIP_GIT_BRANCH_PREFIX=""
+#SPACESHIP_GIT_BRANCH_SUFFIX=""
+#SPACESHIP_GIT_STATUS_PREFIX="["
+#SPACESHIP_GIT_STATUS_SUFFIX="]"
+
+# kubecontext
+SPACESHIP_KUBECONTEXT_PREFIX=""
+#SPACESHIP_KUBECONTEXT_SUFFIX=""
+export SPACESHIP_KUBECONTEXT_COLOR_GROUPS=(
+ red -prod-
+)
+
+# symbols
+SPACESHIP_CHAR_SYMBOL="❯ "
+SPACESHIP_JOBS_SYMBOL="»"
+
+prompt spaceship
