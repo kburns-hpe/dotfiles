1b6d024046b24bb540f5f5743777b0c9adc9ca07
soleblaze <soleblaze@exploits.sexy>
2019-02-16 23:11:56 -0600

    Add functions for todo

--- zsh/todo.zsh
+++ zsh/todo.zsh
@@ -3,5 +3,20 @@ alias ta="todo.py add"
 alias td="todo.py done"
 alias tai="todo.py add Inbox"
 alias tl="todo.py list"
-alias tlp="todo.py list project"
 alias tp="todo.py projects"
+
+function tll() {
+  if [ "$#" -eq 0 ]; then
+    todo.py labels
+  else
+    todo.py list label "$*"
+  fi
+}
+
+function tlp() {
+  if [ "$#" -eq 0 ]; then
+    todo.py projects
+  else
+    todo.py list project "$*"
+  fi
+}
