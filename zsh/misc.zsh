335bce63b7a956759d84a08e5d0abfa3abab572f
Soleblaze <soleblaze@skyshadows.net>
2019-06-16 19:58:42 -0500

    Add fix for ssh-agent forwarding and tmux

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -42,3 +42,11 @@ export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
 export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
+
+# Fix SSH-Agent forwarding and tmux
+if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+  fi
+
+SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
