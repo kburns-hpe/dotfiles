3a7051f1037c2fa30c56907bbf5d7d4b65354c79
Soleblaze <soleblaze@skyshadows.net>
2019-06-29 17:15:14 -0500

    Add ctf aliases

---
+++ zsh/ctf.zsh
@@ -0,0 +1,2 @@
+alias patterncreate "/opt/metasploit-framework/embedded/bin/ruby /opt/metasploit-framework/embedded/framework/tools/exploit/pattern_create.rb -l"
+alias patternoffset "/opt/metasploit-framework/embedded/bin/ruby /opt/metasploit-framework/embedded/framework/tools/exploit/pattern_offset.rb -q"
