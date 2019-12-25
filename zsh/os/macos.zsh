d403369dd809421307b17122779d72a2bd87bf34
soleblaze <soleblaze@exploits.sexy>
2019-12-24 22:48:36 -0600

    Add gpg-agent launch for macos

--- zsh/os/macos.zsh
+++ zsh/os/macos.zsh
@@ -30,3 +30,10 @@ export "SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh"
 # Disable homebrew git token request
 export HOMEBREW_NO_GITHUB_API=TRUE
 
+
+# Launch gpg-agent if it isn't running
+if ! pgrep -q gpg-entry; then
+  gpgconf --launch gpg-agent
+fi
+export SSH_AUTH_SOCKET=$HOME/.gnupg/S.gpg-agent.ssh
+
