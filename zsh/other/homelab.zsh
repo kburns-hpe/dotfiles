35a9c7b42cc28fff360b679565015ee34d0aec82
Soleblaze <soleblaze@skyshadows.net>
2020-05-18 09:25:55 -0500

    Add tail to flux log commands

--- zsh/other/homelab.zsh
+++ zsh/other/homelab.zsh
@@ -7,8 +7,10 @@ alias aves="ansible-vault encrypt_string --vault-password-file ${VAULT_PASS_FILE
 
 alias cephtk="kubectl -n ${CEPH_NAMESPACE} exec -it $(kubectl -n ${CEPH_NAMESPACE} get pod -l 'app=rook-ceph-tools' -o jsonpath='{.items[0].metadata.name}') bash"
 alias fluxctl="fluxctl --k8s-fwd-ns ${FLUX_NAMESPACE}"
-alias fluxlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/flux -f"
-alias fluxhelmlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-operator -f"
+alias fluxlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/flux -f --tail=100"
+alias fluxhelmlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-operator -f --tail=100"
+alias fluxlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/flux -f"
+alias fluxhelmlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-operator -f"
 alias ksealsecret="kubeseal --format=yaml --cert=/home/soleblaze/git/flux/pub-cert.pem"
 alias kubeseal="kubeseal --controller-name=sealed-secrets"
 alias s3cmd="s3cmd --host=${AWS_HOST}"
