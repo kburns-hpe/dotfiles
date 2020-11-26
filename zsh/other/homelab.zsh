f419a103810dcb10b067c7bffe56422d03d79da9
Soleblaze <soleblaze@skyshadows.net>
2020-11-25 21:51:35 -0600

    Update flux log alias for flux 2

--- zsh/other/homelab.zsh
+++ zsh/other/homelab.zsh
@@ -1,4 +1,4 @@
-export FLUX_NAMESPACE="flux"
+export FLUX_NAMESPACE="flux-system"
 export VELERO_NAMESPACE="storage"
 export CEPH_NAMESPACE="storage"
 
@@ -7,10 +7,10 @@ alias aves="ansible-vault encrypt_string --vault-password-file ${VAULT_PASS_FILE
 
 alias cephtk="kubectl -n ${CEPH_NAMESPACE} exec -it $(kubectl -n ${CEPH_NAMESPACE} get pod -l 'app=rook-ceph-tools' -o jsonpath='{.items[0].metadata.name}') bash"
 alias fluxctl="fluxctl --k8s-fwd-ns ${FLUX_NAMESPACE}"
-alias fluxlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/flux -f --tail=100"
-alias fluxhelmlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-operator -f --tail=100"
-alias fluxlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/flux -f"
-alias fluxhelmlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-operator -f"
+alias fluxlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/source-controller -f --tail=100"
+alias fluxhelmlogs="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-controller -f --tail=100"
+alias fluxlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/source-controller -f"
+alias fluxhelmlogsall="kubectl -n ${FLUX_NAMESPACE} logs deployment/helm-controller -f"
 alias ksealsecret="kubeseal --format=yaml --cert=/home/soleblaze/git/flux/pub-cert.pem"
 alias kubeseal="kubeseal --controller-name=sealed-secrets"
 alias s3cmd="s3cmd --host=${AWS_HOST}"
