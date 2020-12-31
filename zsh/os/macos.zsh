175942400824f4f102314e903ed5d52beec19995
Kevin B <soleblaze@skyshadows.net>
2020-12-31 16:26:40 -0600

    Update homebrew paths for m1

--- zsh/os/macos.zsh
+++ zsh/os/macos.zsh
@@ -5,14 +5,14 @@ export BROWSER=safari
 export EDITOR="nvim"
 
 # Set Path
-export PATH="$HOME/bin::/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
+export PATH="$HOME/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
 export PATH="$PATH:$HOME/node_modules/.bin"
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:/Users/soleblaze/.gem/ruby/2.6.0/bin"
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 # Set fpath for zsh-completion (homebrew package)
-fpath=(/usr/local/share/zsh-completions $fpath)
+fpath=(/opt/homebrew/share/zsh/site-functions /usr/local/share/zsh-completions $fpath)
 
 
 # Use neovim instead of vim
