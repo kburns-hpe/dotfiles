94d869336d362ba4a6edd54a97e4f536913ac195
Soleblaze <soleblaze@skyshadows.net>
2020-12-31 23:46:17 -0600

    Revert "Update zsh to deal with kubectl path"
    This reverts commit 94731fb4077a11592d980bae754c31f3777962de.

---
+++ zsh/os/macos.zsh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+# Set Variables
+export BROWSER=safari
+export EDITOR="nvim"
+
+# Set Path
+export PATH="$HOME/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/sbin"
+export PATH="$PATH:$HOME/node_modules/.bin"
+export PATH="$PATH:$HOME/go/bin"
+export PATH="$PATH:/Users/soleblaze/.gem/ruby/2.6.0/bin"
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# Set fpath for zsh-completion (homebrew package)
+fpath=(/opt/homebrew/share/zsh/site-functions /usr/local/share/zsh-completions $fpath)
+
+
+# Use neovim instead of vim
+alias vim="nvim"
+alias vi="nvim"
+
+# Use ctags installed via brew instead of xcode
+alias ctags="/usr/local/bin/ctags"
+
+# Setup SSH keys via gpg
+# export "GPG_TTY=$(tty)"
+# export "SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh"
+
+# Disable homebrew git token request
+export HOMEBREW_NO_GITHUB_API=TRUE
+
+
+# Launch gpg-agent if it isn't running
+# if ! pgrep -q gpg-entry; then
+#   gpgconf --launch gpg-agent
+# fi
+# export SSH_AUTH_SOCKET=$HOME/.gnupg/S.gpg-agent.ssh
+
